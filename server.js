867b22286e479d84217cb3487d716c6efb3726e8
[Dan Brown] <[thedanbrown@gmail.com]>
2022-11-15 21:26:16 -0500

    port issue?

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 //enabling express
 const app = express();
 //setting our note json file as a variable
